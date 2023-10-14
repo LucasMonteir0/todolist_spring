2b2c6458a67c3027e6b42564aaab2d93a842e50e
Lucas Monteiro <lucasmonteirob1@outlook.com>
2023-10-14 18:08:50 -0300

    comment test

--- src/test/java/com/example/todolist/TodolistApplicationTests.java
+++ src/test/java/com/example/todolist/TodolistApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.todolist;
+// package com.example.todolist;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TodolistApplicationTests {
+// @SpringBootTest
+// class TodolistApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
